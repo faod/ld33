64c62582f897b5bf9066d283e044f48632ae0efa
baylej <baylej@mrhide.fr>
2015-08-22 21:25:51 +0200

    Objects WIP

---
+++ src/objects/object.cpp
@@ -0,0 +1,26 @@
+#include "object.hpp"
+
+glm::vec2 Object::getPosition() {
+	return this->position;
+}
+
+void Object::updatePosition(glm::vec2 &delta) {
+	this->position += delta;
+}
+
+void Object::setPosition(glm::vec2 &pos) {
+	this->position = pos;
+}
+
+bool Object::collide(Object &other) { // TODO
+	return false;
+}
+
+ConvexHull* Object::getConvexHull() { // TODO
+	return NULL;
+}
+
+glm::vec4 Object::getBoundingBox() { // TODO
+	glm::vec4 res;
+	return res;
+}
--- src/objects/object.hpp
+++ src/objects/object.hpp
@@ -4,47 +4,32 @@
 
 #include <glm/glm.hpp>
 
-
 struct ConvexHull {
 	int len;
 	glm::vec2 *points;
 };
 
 class Object {
-public:
-	/// Returns the global position in pixels
-	virtual glm::vec2 getPosition() = 0;
-
-	/// Sets the global position
-	virtual void setPosition(glm::vec2 &pos) = 0;
-
-	/// Append the given coordinates to this object's position
-	virtual void updatePosition(glm::vec2 &delta) = 0;
-
-	/// Returns `true` if the given object collides with this object
-	virtual bool collide(Object &other) = 0;
-
-	/// Returns the convex hull for this object for collision calculation purposes
-	virtual ConvexHull* getConvexHull() = 0;
-};
-
-class baseObject : public Object {
+protected:
 	glm::vec2 position;
 public:
 	/// Returns the global position in pixels
-	inline glm::vec2 getPosition() { return position; };
-
-	/// Sets the global position
-	inline void setPosition(glm::vec2 &pos) { this->position = pos; };
+	virtual glm::vec2 getPosition();
 
 	/// Append the given coordinates to this object's position
-	inline void updatePosition(glm::vec2 &delta) { this->position += delta; };
+	virtual void updatePosition(glm::vec2 &delta);
 
-    /// true if given object collides with this
-    virtual bool collide(Object &other) = 0;
+	/// Sets the global position
+	virtual void setPosition(glm::vec2 &pos);
 
-    /// return the convex hull for this object
-    virtual ConvexHull* getConvexHull() = 0;
+	/// Returns `true` if the given object collides with this object
+	virtual bool collide(Object &other);
+
+	/// Returns the convex hull for this object for collision calculation purposes
+	virtual ConvexHull* getConvexHull();
+
+	/// Returns the axis aligned bounding box for this object
+	virtual glm::vec4 getBoundingBox();
 };
 
 #endif // OBJ_HPP
