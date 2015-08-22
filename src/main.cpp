678a178f474670604024017968ca6757e7ec3b69
baylej <baylej@mrhide.fr>
2015-08-22 12:57:34 +0200

    Removed OpenGL code

--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,6 @@
 #include "main.hpp"
 #include "failure.hpp"
 
-#include "3D/glw.hpp"
-#include "3D/types.hpp"
-#include "3D/transfo.hpp"
-#include "3D/camera.hpp"
-#include "3D/shaders.hpp"
-#include "3D/geometry.hpp"
-
 using std::cerr;
 using std::endl;
 
@@ -38,31 +31,8 @@ Main::Main(int screen_w, int screen_h) {
 	//al_set_new_display_flags(ALLEGRO_OPENGL | ALLEGRO_OPENGL_3_0); // Modern OpenGL (compatibility context)
 	//al_set_new_display_flags(ALLEGRO_OPENGL | ALLEGRO_OPENGL_3_0 | ALLEGRO_PROGRAMMABLE_PIPELINE); // using Allegro's Shader Manager
 
-	// Multisampling (antialiasing)
-	al_set_new_display_option(ALLEGRO_SAMPLE_BUFFERS, 1, ALLEGRO_SUGGEST);
-	al_set_new_display_option(ALLEGRO_SAMPLES, 4, ALLEGRO_SUGGEST); // 4x
-	
 	if (display = al_create_display(screen_w, screen_h), !display) throw Failure("failed to create display!");
-	
-	// Clears the GL error buffer
-	int err;
-	while (err = glGetError(), err != GL_NO_ERROR) {
-		cerr << "Gl Error : " << err << endl;
-	}
 
-	// Print OpenGL version
-	cerr << glGetString(GL_VERSION) << endl;
-
-	// Enable Depth Test (tells OpenGL which sample must be drawn in the framebuffer) (3D)
-	glEnable(GL_DEPTH_TEST);
-	glDepthFunc(GL_LEQUAL);
-	glClearDepth(1.0);
-
-	// Enable culling (render visible triangles only) (3D)
-	glEnable(GL_CULL_FACE);
-	//glCullFace(GL_BACK);
-	glFrontFace(GL_CCW); // CounterClockWise triangles vertices
-	
 	if (!al_install_keyboard()) throw Failure("failed to initialize the keyboard!");
 	if (!al_install_mouse()) throw Failure("failed to initialize the mouse!");
 	//if (!al_install_joystick()) throw Failure("failed to initialize the joystick!");
@@ -125,37 +95,16 @@ Main::~Main() {
 int main(int argc, char *argv[]) {
 	try {
 		Main m(800, 600);
-		Matrix identity, model;
-		Camera camera;
-		Shaders shaders;
-		Cube c;
-		AxisDrawer ad(10.);
 		ALLEGRO_COLOR grey  = al_map_rgb(55, 55, 55);
-
-		shaders.loadDefaultShaders();
-		GLuint model_mat = glGetUniformLocation(shaders.defProgId, "model_m4");
-		
-		camera.frustum(800, 600, .1, 100., 45.);
-		camera.setPosition(-5., -5., -5.);
-		camera.setOrientation(ALLEGRO_PI/5., -ALLEGRO_PI/4., 0.);
-		camera.foenum();
-		camera.loadProjectionMatrix(shaders.defProgId);
-		camera.loadViewMatrix(shaders.defProgId);
 
 		ALLEGRO_EVENT ev;
 		int step = 0;
 		al_start_timer(m.animationTimer);
 		bool loop = true;
 		while (al_wait_for_event(m.animationEQ, &ev), loop) {
-			glClear(GL_DEPTH_BUFFER_BIT);
 			al_clear_to_color(grey);
 
-			model = RotationMatrix(1, 0.005 * step) * RotationMatrix(0, 0.002 * step); step++;
-			glUniformMatrix4dv(model_mat, 1, GL_FALSE, model.mat);
-			c.draw();
-
-			glUniformMatrix4dv(model_mat, 1, GL_FALSE, identity.mat);
-			ad.draw();
+			// TODO
 
 			al_flip_display();
 
