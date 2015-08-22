cab9edc250e7fa546fbc445e35d91ef77bd7ba18
Thomas Medioni <medioni.thomas@gmail.com>
2015-08-22 18:49:02 +0200

    add vector of objects into the game

--- src/game.hpp
+++ src/game.hpp
@@ -5,8 +5,12 @@
 #ifndef GAME_HPP
 #define GAME_HPP
 
+#include <vector>
+#include <memory>
+
 #include "main.hpp"
 #include "map.hpp"
+#include "objects/object.hpp"
 
 class Game {
 public:
@@ -19,6 +23,7 @@ public:
 private:
     Main& main_;
     Map map_;
+    std::vector<std::shared_ptr<Object>> objects_;
 };
 
 #endif
