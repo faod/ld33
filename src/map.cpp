32539a6d69007e7a48e162371ec4348fece577a5
Thomas Medioni <medioni.thomas@gmail.com>
2015-08-22 15:27:28 +0200

    perlin implement

---
+++ src/map.cpp
@@ -0,0 +1,36 @@
+
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+#include <allegro5/allegro_font.h>
+
+#include "map.hpp"
+#include "noise/perlin.hpp"
+
+#include <algorithm>
+
+Map::Map(int width, int height) : width_(width), height_(height)
+{
+    bm_ = al_create_bitmap(width_, height_);
+    al_set_target_bitmap(bm_);
+
+    const float resolution = 150.0;
+    const float max = static_cast<float>(std::max(width_, height_));
+
+    for(int i = 0; i < width_; ++i)
+    {
+        for(int j = 0; j < height_; ++j)
+        {
+            float vec[2] = {(i + 0.5) / max * resolution, (j + 0.5) / max * resolution};
+            al_draw_pixel(i, j , al_map_rgb_f(noise2(vec), 0., 0.));
+        }
+    }
+    al_set_target_backbuffer(al_get_current_display());
+}
+
+void Map::draw()
+{
+    al_draw_scaled_bitmap(bm_, 0, 0, width_, height_, 0, 0, width_ * 10, height_ * 10, 0);
+}
---
+++ src/map.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef MAP_HPP
+#define MAP_HPP
+
+
+class Map
+{
+    public:
+        Map(int width, int height);
+
+        void draw();
+    private:
+        int width_;
+        int height_;
+        ALLEGRO_BITMAP *bm_;
+
+};
+
+#endif //MAP_HPP
