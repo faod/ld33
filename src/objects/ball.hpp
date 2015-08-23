4f2bd45c8bb5364a89166e4ae17add5a0242f9eb
Thomas Medioni <medioni.thomas@gmail.com>
2015-08-23 22:54:00 +0200

    Make Ball class shared_from_this
    Allows Ball class to return a shared_ptr and will share ownership with
    previous shared_ptr

--- src/objects/ball.hpp
+++ src/objects/ball.hpp
@@ -2,11 +2,14 @@
 #ifndef BALL_HPP
 #define BALL_HPP
 
+#include <memory>
+
 #include "object.hpp"
+
 
 class Game;
 
-class Ball : public BoxObject
+class Ball : public BoxObject, public std::enable_shared_from_this<Ball>
 {
     public:
         Ball(glm::vec2 position, Game &g, float direction);
