973a296498a5750405440c32a1457d15e8b2308e
Thomas Medioni <medioni.thomas@gmail.com>
2015-08-24 02:16:12 +0200

    wip

---
+++ src/menu/menu.cpp
@@ -0,0 +1,17 @@
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+#include <allegro5/allegro_font.h>
+
+
+
+
+#include "menu.hpp"
+
+
+void Menu::processInput(ALLEGRO_EVENT ev)
+{
+
+}
---
+++ src/menu/menu.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef MENU_HPP
+#define MENU_HPP
+
+
+class MenuItem
+{
+    
+};
+
+class Menu
+{
+    public:
+               
+
+        void update();        
+        void processInput(ALLEGRO_EVENT ev); 
+        void draw();
+
+    private:
+                
+};
+
+#endif
