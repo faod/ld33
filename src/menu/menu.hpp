aea0ecb0adcf816c623e5fefff05aedc41123bc1
Thomas Medioni <medioni.thomas@gmail.com>
2015-08-24 13:25:22 +0200

    fix trailing space

--- src/menu/menu.hpp
+++ src/menu/menu.hpp
@@ -9,18 +9,18 @@
 class Menu
 {
     public:
-        Menu(int width, int height); 
+        Menu(int width, int height);
         ~Menu();
 
-        void update();        
-        void processInput(ALLEGRO_EVENT ev); 
+        void update();
+        void processInput(ALLEGRO_EVENT ev);
         void draw();
 
     private:
         int width_;
         int height_;
         ALLEGRO_BITMAP* bm_;
-                
+
 };
 
 #endif
